974c2b157443df55d31cab273e7bd6d3cd426718
maafk <maafk@users.noreply.github.com>
2020-07-05 20:37:56 -0400

    first pass at github actions

---
+++ test_update_item.py
@@ -0,0 +1,52 @@
+import boto3
+from unittest import TestCase, mock
+
+from update_item import UpdateItem
+import os
+import json
+
+client = boto3.client(
+    'dynamodb', 
+    endpoint_url='http://localhost:8000',
+    aws_access_key_id='ABC123',
+    aws_secret_access_key='DEF456')
+
+
+TEST_TABLE_NAME = "SampleTestTableKittens"
+TEST_SITE_URL = "rainbowkittensloths.com"
+
+class DynamoTest(TestCase):
+    def setUp(self):
+        # Create table
+        client.create_table(
+            TableName=TEST_TABLE_NAME,
+            KeySchema=[
+                {
+                    "AttributeName": "site_url",
+                    "KeyType": "HASH"
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    "AttributeName": "site_url",
+                    "AttributeType": "S"
+                }
+            ],
+            BillingMode="PAY_PER_REQUEST"
+        )
+
+    def tearDown(self):
+        client.delete_table(
+            TableName=TEST_TABLE_NAME
+        )
+
+    @mock.patch('update_item.TABLE_NAME', TEST_TABLE_NAME)
+    @mock.patch('update_item.SITE_URL', TEST_SITE_URL)
+    def test_update(self):
+        test_obj = UpdateItem(client)
+        response = test_obj.update_count()
+        body_response = json.loads(response["body"])
+        # assert count was incremented by one
+        self.assertEqual(200, response['statusCode'])
+        # assert incremented by 1
+        self.assertEqual("1", body_response["Visit_Count"])
---
+++ update_item.py
@@ -0,0 +1,47 @@
+import boto3
+import json
+import os
+
+TABLE_NAME = os.getenv("TABLE_NAME", 'sample-table')
+SITE_URL = os.getenv("SITE_URL", 'example.com')
+
+client = None
+
+class UpdateItem(object):
+    
+    def __init__(self, client):
+        self.client = client
+
+    def update_count(self):
+        print(TABLE_NAME)
+        response = self.client.update_item(
+            TableName=TABLE_NAME,
+            Key={
+                'site_url': {
+                    'S': SITE_URL
+                }
+            },
+            ReturnValues='UPDATED_NEW',
+            UpdateExpression='ADD visit_count :val',
+            ExpressionAttributeValues={
+                ":val": {
+                    "N": "1"
+                }
+            }
+        )
+
+        visit_count = response["Attributes"]["visit_count"]["N"]
+        return {
+            "statusCode": 200,
+            "headers": {
+                "Access-Control-Allow-Headers" : "*"
+            },
+            "body": json.dumps({"Visit_Count": visit_count})
+        }
+
+def lambda_handler(event, context):
+    global client
+    if not client:
+        client = boto3.client('dynamodb')
+    obj = UpdateItem(client)
+    obj.update_count()
